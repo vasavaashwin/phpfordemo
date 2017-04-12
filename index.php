8ba6f7e30257a5f42594057d24153b7079efc590
vasavaashwin <ashwin.vasava@gmail.com>
2017-04-12 12:08:52 +0530

    Update index.php

--- index.php
+++ index.php
@@ -15,20 +15,31 @@
 </head>
 
 <body>
-<form method = "POST">
-     <div id="login-box">
-  <div >
-    <h1>Sign up</h1>
-    
-    <input type="text" name="name" placeholder="Name" />
-    <input type="text" name="email" placeholder="E-mail" />
-    <input type="text" name="number" placeholder="Number" />
-    
-    <input type="submit" name="signup_submit" value="Sign me up" />
+  <form class="form-horizontal" method = "POST">
+   <div class="form-group">
+    <label for="inputName3" class="col-sm-2 control-label">Name</label>
+    <div class="col-sm-10">
+      <input type="text" class="form-control" id="inputName3" placeholder="Name">
+    </div>
   </div>
-
-</div>
- </form>
+  <div class="form-group">
+    <label for="inputEmail3" class="col-sm-2 control-label">Email</label>
+    <div class="col-sm-10">
+      <input type="email" class="form-control" id="inputEmail3" placeholder="Email">
+    </div>
+  </div>
+ <div class="form-group">
+    <label for="inputNumber3" class="col-sm-2 control-label">Number</label>
+    <div class="col-sm-10">
+      <input type="text" class="form-control" id="inputNumber3" placeholder="Number">
+    </div>
+  </div>
+  <div class="form-group">
+    <div class="col-sm-offset-2 col-sm-10">
+      <button type="submit" class="btn btn-default">Let's Begin</button>
+    </div>
+  </div>
+</form>
 </body>
 <script src="http://code.jquery.com/jquery.js"></script>
   <script src="js/bootstrap.min.js"></script>
