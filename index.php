36ff84d69df8217bf13a878be55aa970ad483da1
vasavaashwin <ashwin.vasava@gmail.com>
2017-04-12 11:39:53 +0530

    Update index.php

--- index.php
+++ index.php
@@ -17,7 +17,7 @@
 <body>
 <form method = "POST">
      <div id="login-box">
-  <div class="left">
+  <div >
     <h1>Sign up</h1>
     
     <input type="text" name="name" placeholder="Name" />
@@ -26,15 +26,7 @@
     
     <input type="submit" name="signup_submit" value="Sign me up" />
   </div>
-  
-  <div class="right">
-    <span class="loginwith">Sign in with<br />social network</span>
-    
-    <button class="social-signin facebook">Log in with facebook</button>
-    <button class="social-signin twitter">Log in with Twitter</button>
-    <button class="social-signin google">Log in with Google+</button>
-  </div>
-  <div class="or">OR</div>
+
 </div>
  </form>
 </body>
