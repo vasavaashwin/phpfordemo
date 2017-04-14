980a7728e9922ee13548ac386c19757a48c9e91c
vasavaashwin <ashwin.vasava@gmail.com>
2017-04-14 17:04:53 +0530

    changes register

--- register.php
+++ register.php
@@ -1,9 +1,8 @@
 <?php
-$vcap_services = json_decode($_ENV["VCAP_SERVICES" ]);
+$$vcap_services = json_decode($_ENV["VCAP_SERVICES" ]);
 $db = $vcap_services->{'mysql-5.5'}[0]->credentials;
 $mysql_database = $db->name;
 $mysql_port=$db->port;
-//$mysql_server_name ='${db->host}:${db->port}';
 $mysql_server_name =$db->host . ':' . $db->port;
 $mysql_username = $db->username; 
 $mysql_password = $db->password; 
@@ -12,28 +11,32 @@ $con = mysql_connect($mysql_server_name, $mysql_username, $mysql_password);
 if (!$con){
     die ('connection failed' . mysql_error());
 }
-mysql_select_db($mysql_database,$con);  
+mysql_select_db($mysql_database,$con);   
 $name=$_POST['name']; // Fetching Values from URL.
 $email=$_POST['email'];
 $number= $_POST['number']; 
 // Check if e-mail address syntax is valid or not
 $email = filter_var($email, FILTER_SANITIZE_EMAIL); // Sanitizing email(Remove unexpected symbol like <,>,?,#,!, etc.)
-if (!filter_var($email, FILTER_VALIDATE_EMAIL)){
+/*if (!filter_var($email, FILTER_VALIDATE_EMAIL)){
 echo "Invalid Email.......";
 }else{
 $result = mysql_query("SELECT * FROM user WHERE email='$email'");
 $data = mysql_num_rows($result);
-if(($data)==0){
-$query = mysql_query("insert into user(name, email, mobile_number,social_profile,status) values ('$name', '$email', '$number','facebook',1)"); // Insert query
-if($query){
-echo "You have Successfully Registered.....";
-}else
-{
-echo "Error....!!";
+if(($data)==0){ */
+$strsq0 = "INSERT INTO user ( name, email, mobile_number,social_profile,status) VALUES
+('$name', '$email', '$number','facebook',1);";
+$result0 = mysql_query ( $strsq0 );
+if ($result0) {
+	echo "insert success!";
+} else {
+	echo "Cannot insert into the data table; check whether the table is created, or the database is active.";
 }
+/*
 }else{
 echo "This email is already registered, Please try another email...";
 }
 }
-mysql_close ($con);
+*/
+
+mysql_close();
 ?>
