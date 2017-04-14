08085de8db525d5be36eac2985140521e70d03b2
vasavaashwin <ashwin.vasava@gmail.com>
2017-04-14 16:35:13 +0530

    changes

--- register.php
+++ register.php
@@ -1,9 +1,14 @@
 <?php
+echo '<h2>VCAP_SERVICE Environment variable</h2>';
+echo "----------------------------------" . "</br>";
+$key = "VCAP_SERVICES";
+$value = getenv ( $key );
+echo $key . ":" . $value . "</br>";
+echo "----------------------------------" . "</br>";
 $vcap_services = json_decode($_ENV["VCAP_SERVICES" ]);
 $db = $vcap_services->{'mysql-5.5'}[0]->credentials;
 $mysql_database = $db->name;
 $mysql_port=$db->port;
-//$mysql_server_name ='${db->host}:${db->port}';
 $mysql_server_name =$db->host . ':' . $db->port;
 $mysql_username = $db->username; 
 $mysql_password = $db->password; 
@@ -12,7 +17,10 @@ $con = mysql_connect($mysql_server_name, $mysql_username, $mysql_password);
 if (!$con){
     die ('connection failed' . mysql_error());
 }
-mysql_select_db($mysql_database,$con);  
+mysql_select_db($mysql_database,$con);   
+
+exit;
+/*
 $name=$_POST['name']; // Fetching Values from URL.
 $email=$_POST['email'];
 $number= $_POST['number']; 
@@ -36,4 +44,5 @@ echo "This email is already registered, Please try another email...";
 }
 }
 mysql_close ($con);
+*/
 ?>
