7290e280e2e6b7c59dda665427bc7d74c9f5c949
vasavaashwin <ashwin.vasava@gmail.com>
2017-04-15 14:24:52 +0530

    clearDB to PostgreSQL

--- register.php
+++ register.php
@@ -1,73 +1,59 @@
 <?php
+//phpinfo();
 error_reporting(E_ALL);
 ini_set('display_errors', 1);
-if (getenv("VCAP_SERVICES")===false) {
-$db = 'XXXX';
-define("APP_DB_SCHEMA", $db);
-define("APP_DB_STRING", 'xxxx');
-//define("APP_DB_HOST", 'xxxx');
-//define("APP_DB_PORT", "xxxx");
-//define("APP_DB_USERNAME", 'xxxx');
-//define("APP_DB_PASSWORD", "xxxx");
 
-} else {
+//postgres://admin:WUHZQQUELISSUWGO@sl-us-dal-9-portal.6.dblayer.com:22912/compose
 
-// getting VCAP configuration
-$services = getenv("VCAP_SERVICES");
-$services_json = json_decode($services, true);
-$pgsql_config = $services_json["compose-for-postgresql"][0]["credentials"];
-
-define("APP_DB_SCHEMA", $pgsql_config["name"]);
-define("APP_DB_STRING", $pgsql_config["uri"])
-//define("APP_DB_HOST", $pgsql_config["host"]);
-//define("APP_DB_PORT", $pgsql_config["port"]);
-//define("APP_DB_USERNAME", $pgsql_config["user"]);
-//define("APP_DB_PASSWORD", $pgsql_config["password"]);
-}
-
-//$dbConnectionString = "host=" . APP_DB_HOST . " port=" . APP_DB_PORT . " dbname=" . APP_DB_SCHEMA . " user=" . APP_DB_USERNAME . " password=" . APP_DB_PASSWORD;
-$dsn = APP_DB_STRING;
- 
+//$dsn = 'postgres://admin:WUHZQQUELISSUWGO@sl-us-dal-9-portal.6.dblayer.com:22912/compose';
+$dsn = 'pgsql:dbname=compose;user=admin;password=WUHZQQUELISSUWGO;host=sl-us-dal-9-portal.6.dblayer.com;port=22912;';
 try{
 	// create a PostgreSQL database connection
 	$conn = new PDO($dsn);
+	//$conn = new PDO("pgsql:dbname=compose;host=sl-us-dal-9-portal.6.dblayer.com", 'admin', 'WUHZQQUELISSUWGO'); 
+	//$conn = pg_connect($dsn);
  
 	// display a message if connected to the PostgreSQL successfully
 	if($conn){
-		echo "Connected to the <strong>$db</strong> database successfully!";
+		echo "Connected to the <strong>compose</strong> database successfully!";
 	}
 }catch (PDOException $e){
+	var_dump($e);
 	// report error message
-	echo $e->getMessage();
+	echo $e->getMessage(); 
 }
-
-/*
-   
+	
 $name=$_POST['name']; // Fetching Values from URL.
 $email=$_POST['email'];
-$number= $_POST['number']; 
-// Check if e-mail address syntax is valid or not
-$email = filter_var($email, FILTER_SANITIZE_EMAIL); // Sanitizing email(Remove unexpected symbol like <,>,?,#,!, etc.)
-if (!filter_var($email, FILTER_VALIDATE_EMAIL)){
-echo "Invalid Email.......";
-}else{
-$result = mysql_query("SELECT * FROM user WHERE email='$email'");
-$data = mysql_num_rows($result);
-if(($data)==0){ 
-$strsq0 = "INSERT INTO user ( name, email, mobile_number,social_profile,status) VALUES
-('$name', '$email', '$number','facebook',1);";
-$result0 = $mysqli->query ( $strsq0 );
-if ($result0) {
-	echo "insert success!";
-} else {
-	echo "Cannot insert into the data table; check whether the table is created, or the database is active.";
-}
+$number= $_POST['number']; 	
+	
+	 try {	
+$statement = $conn->prepare("INSERT INTO appusers(username,
+            email,
+            mobile_number,
+            social_profile,
+            status)
+    VALUES(:username, :email, :mobile_number,:social_profile,:status)");
+$statement->execute(array(
+    "username" => $name,
+    "email" => $email,
+    "mobile_number" => $number,
+	"social_profile" => "instagram",
+	"status" => "1"
+));		
+		
+   //echo $statement->queryString;die;
+   $statement->debugDumpParams();echo '<hr>';
 
-}else{
-echo "This email is already registered, Please try another email...";
-}
-}
-*/
+ } catch(PDOException $e) {
+        echo $e->getMessage();
+    }
 
- mysqli_close();
+
+
+$sql= "SELECT * FROM appusers";
+$stmt = $conn->prepare($sql);
+$stmt->execute();
+$result = $stmt->fetchAll();
+return json_encode($result);	
 ?>
