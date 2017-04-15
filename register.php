4e8f33ace452015102a61a277e7cd359fcd21b57
vasavaashwin <ashwin.vasava@gmail.com>
2017-04-15 15:23:25 +0530

    pdo

--- register.php
+++ register.php
@@ -5,11 +5,11 @@ ini_set('display_errors', 1);
 
 //postgres://admin:WUHZQQUELISSUWGO@sl-us-dal-9-portal.6.dblayer.com:22912/compose
 
-$dsn = 'postgres://admin:WUHZQQUELISSUWGO@sl-us-dal-9-portal.6.dblayer.com:22912/compose';
-//$dsn = 'pgsql:dbname=compose;user=admin;password=WUHZQQUELISSUWGO;host=sl-us-dal-9-portal.6.dblayer.com;port=22912;';
+//$dsn = 'postgres://admin:WUHZQQUELISSUWGO@sl-us-dal-9-portal.6.dblayer.com:22912/compose';
+$dsn = 'pgsql:dbname=compose;user=admin;password=WUHZQQUELISSUWGO;host=sl-us-dal-9-portal.6.dblayer.com;port=22912;';
 try{
 	// create a PostgreSQL database connection
-	$conn = new PDO($dsn);
+	$conn = new \PDO($dsn);
 	//$conn = new PDO("pgsql:dbname=compose;host=sl-us-dal-9-portal.6.dblayer.com", 'admin', 'WUHZQQUELISSUWGO'); 
 	//$conn = pg_connect($dsn);
  
